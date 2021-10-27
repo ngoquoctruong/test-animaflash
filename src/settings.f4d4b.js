6bf60e868fe9a4ed473b2d516a5564d36c7d8c79
ngoquoctruong <ngoquocturong302@gmail.com>
2021-10-27 16:10:36 +0700

    “update”

---
+++ src/settings.f4d4b.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/landing.fire",orientation:"",jsList:["assets/scripts/analytic/gta-amplitude.f658d.js"],bundleVers:{internal:"3c089",resources:"65064",main:"77f99"}};
